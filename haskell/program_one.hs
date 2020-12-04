f0bf7f88d6ed80b38bc310dee4215088244931dc
cullinap <cullinap@gmail.com>
2020-12-04 00:03:19 -0500

    haskell: write urself a scheme

---
+++ haskell/program_one.hs
@@ -0,0 +1,37 @@
+module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+data ListVal = Atom String
+             | List [LispVal]
+             | DottedLIst [ListVal] ListVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do 
+                char '"'
+                x <- man (noneOf "\"")
+                char '"'
+                return $ String x
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+main :: IO ()
+main = do
+    (expr:_) <- getArgs
+    putStrLn (readExpr expr)
+
+
+
+
