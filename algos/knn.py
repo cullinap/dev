6328809bb4a012dd9bcb124e4155f4507d860830
cullinap <cullinap@gmail.com>
2020-12-13 22:08:11 -0500

    add in sorter that finds indexs and returns most mber of group

--- algos/knn.py
+++ algos/knn.py
@@ -1,18 +1,19 @@
 import numpy as np
-
+from collections import Counter
 
 
 def kNearestNeighbors(data, predict, k=3):
-    distances = []
     
+    distances = []
     for i in data.keys():
         for j in data[i]:
             dist = np.linalg.norm(np.array(j) - np.array(predict))
-            print(dist)
+            distances.append([dist,i])
 
-
+    votes = [i[1] for i in sorted(distances)[:k]]
+    return Counter(votes).most_common(1)[0][0]
 
 if __name__ == '__main__':
-    data = {'1':[[1,2],[2,3]]}
-    kNearestNeighbors(data, [3,4])
+    data = {'1':[[1,2],[2,3]], '2':[[8,7],[7,5]]}
+    print(kNearestNeighbors(data, [3,4]))
 
