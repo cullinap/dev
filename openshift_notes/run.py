532f9c2426de8544faf1136c1ae5dee5e8d8e9f1
cullinap <cullinap@gmail.com>
2020-12-30 12:50:10 -0500

    added login session feature and displays name of current user session in quiz page

--- openshift_notes/run.py
+++ openshift_notes/run.py
@@ -1,11 +1,15 @@
 import os
+import json
 from flask import Flask, render_template, request, redirect, flash, url_for, session
 from datetime import timedelta
+import sqlalchemy
 
 
 app = Flask(__name__)
 app.secret_key = "temp_key" # set the secret key 
 app.permanent_session_lifetime = timedelta(days=300) # establish session lifetime
+quiz_data = 'data/quiz_data.json'
+
 
 # @app.route('/', methods=["GET","POST"])
 # def login():
@@ -29,14 +33,44 @@ def login():
 		session.permanent = True # set permanent session
 		user = request.form['nm'] # take data from login.html form 
 		session['user'] = user # set the session user name to name from request form
+		session['url'] = 2
 		flash("Login Successful!")
-		return redirect(url_for('user')) # go to user page with session info
+		return redirect(url_for('quiz_app')) # go to user page with session info
 	else:
 		if 'user' in session:
+			session['url'] = 1
 			flash("Already logged in!")
-			return redirect(url_for('user')) # if you already logged in
+			return redirect(url_for('quiz_app')) # if you already logged in
 
 		return render_template('login.html') # if method not post go back to login
+
+
+def match_page_info_with_url(json_file, number):
+	riddle = {}
+	with open(json_file, "r") as json_data:
+		data = json.load(json_data)
+		for obj in data:
+			if obj['url'] == str(number):
+				riddle = obj
+
+	return riddle 
+
+
+@app.route('/quiz')
+def quiz_app():
+	total = session['url'] - 1
+	session['url'] = 1
+	user = session["user"]
+	with open(quiz_data, "r") as json_data:
+		data = json.load(json_data)
+		for obj in data:
+			riddle = obj
+
+
+	#riddle = match_page_info_with_url(quiz_data, session['url'])
+
+	return render_template('member.html', riddle=riddle, user=user, total=total)
+
 
 # user page
 @app.route('/user')
