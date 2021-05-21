39c0fb6a63ea54267d43b1c35f51d311cee6236d
cullinap <cullinap@gmail.com>
2021-05-21 08:07:29 -0400

    add linkedlist

---
+++ practice/covid.sql
@@ -0,0 +1,21 @@
+
+create table covid (
+	"FIPS" TEXT,
+	"Admin2" TExT,
+	"Province_State" TEXT,
+	"Country_Region" TEXT,
+	"Last_Update" TEXT,
+	"Lat" integer,
+	"Long" integer,
+	"Confirmed" TEXT,
+	"Deaths" integer,
+	"Recovered" integer,
+	"Active" integer,
+	"Combined_Key" text,
+	"Incidence_rate" text,
+	"Case_Fatality_Ratio" text
+)
+
+
+
+
