38a32cfcf0cf2e826602a048cbe76d3e6deb6ce1
cullinap <cullinap@gmail.com>
2021-05-24 08:41:03 -0400

    add random question generator

---
+++ practice/interview_questions.py
@@ -0,0 +1,6 @@
+questions = {
+    'what is p-value?': 'A p-value is a measure of the probability that an observed difference could have occurred just by random chance.',
+    'what is a type 1 error': 'false positive: rejecting the null hypothesis when it’s actually true. The test result says you have coronavirus, but you actually don’t',
+    'what is a type 2 error': 'false negative: not rejecting the null hypothesis when it’s actually false. The test result says you don’t have coronavirus, but you actually do.'
+}
+
---
+++ practice/warm_up.py
@@ -0,0 +1,17 @@
+questions = {
+    0:'make a dataframe from a list and a dictionary',
+    1:'assign the columns, index, and data to varaibles and display',
+    2:'display the dataframe\'s data types, value_counts, and info',
+    3:'select the same column with loc and iloc, and then print out all types in the column',
+    4:'return unique values from the column, and the frequencies of values in the column',
+    5:'return the summary statistics of the column',
+    6:'find the size and shape of the data',
+    7:'check to see if there are missing values in the dataset',
+    8:'rename index and column name',
+    9:'create a column/delete a column',
+    10:'sql: create a table and insert values',
+    11:'sql: use like to select string match and/or/not',
+    12:'create a vector and matrix in pytorch and numpy',
+    13:'calculate the dot product of two vectors with pytorch',
+    14:'multiple two matrices in pytorch',
+}
